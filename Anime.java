380aca6e469f155e081efac3deae5ca4ee95d22a
julian_paulo <julianpaulo157@gmail.com>
2023-03-07 09:00:38 -0300

    commit inicial

---
+++ Anime.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+
+public class Anime {
+
+    public int id;
+    public String nome;
+    public LocalDate dataLancamento;
+    public String classificacoIndicativa;
+    public String genero;
+    public String autor;
+    public int quantidadeEpisodios;
+    public int temporadas;
+}
---
+++ AnimeTeste.java
@@ -0,0 +1,15 @@
+public class AnimeTeste {
+
+    public static void main(String[] args) {
+    //Classe nomeObjeto = new Classe();
+    Anime meuAnime = new Anime();
+    meuAnime.nome ="Super11";
+    meuAnime.quantidadeEpisodios = 32;
+    meuAnime.temporadas = 1;
+
+    Anime meuAnimePreferido = new Anime();
+    meuAnimePreferido.nome = "Naruto";
+    meuAnimePreferido.quantidadeEpisodios = 327;
+    meuAnimePreferido.temporadas = 5;
+    }
+}
